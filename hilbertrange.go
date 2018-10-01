9ae33a945adf6e2a0776869c86013b3cb2cd9587
Jason R. Surratt <jason.surratt@digitalglobe.com>
2018-10-01 13:26:28 -0600

    [GIS-12] Add method to decompose region to cells

--- hilbertrange.go
+++ hilbertrange.go
@@ -1,9 +1,5 @@
 package sfc
 
-import (
-	"sync"
-)
-
 // Intersecter provides method for determining the relationship between a
 // region (this) and bounds.
 //
@@ -19,6 +15,12 @@ type Intersecter interface {
 
 // CellIterator is a function that iterates to the next cell in cellIterator
 type CellIterator func() bool
+
+// Cell represents a specific hilbert curve value at a specific tier/order
+type Cell struct {
+	Value Bitmask
+	Tier  uint32
+}
 
 // cellIterator returns a function that enables iterating over 2 ^ dim cells
 // at a given tier/location.
@@ -64,13 +66,10 @@ type decomposeCall struct {
 	minTier uint32
 	maxTier uint32
 	bounds  Bounds
-	ranges  chan Range
 	region  Intersecter
-	err     chan error
-	wg      *sync.WaitGroup
 }
 
-// DecomposeRegion breaks a region up into a series of hilbert value ranges.
+// DecomposeRanges breaks a region up into a series of hilbert value ranges.
 //
 // minTier - The minimum tier in the hilbert curve to start the decomposition.
 // Setting this too high may result in a large number of ranges.
@@ -78,7 +77,7 @@ type decomposeCall struct {
 // maxTier - The maximum tier to recurse down to during the decomposition.
 // Setting maxTier to a high value may results in a very large number of
 // ranges.
-func (hc *Hilbert) DecomposeRegion(minTier, maxTier uint32,
+func (hc *Hilbert) DecomposeRanges(minTier, maxTier uint32,
 	region Intersecter) (Ranges, error) {
 
 	cell := make(Point, hc.dim, hc.dim)
@@ -86,18 +85,15 @@ func (hc *Hilbert) DecomposeRegion(minTier, maxTier uint32,
 
 	dc := decomposeCall{
 		bounds:  Bounds{Min: cell.Clone(), Max: cell.Clone()},
-		err:     make(chan error),
 		minTier: minTier,
 		maxTier: maxTier,
-		ranges:  make(chan Range),
 		region:  region,
-		wg:      &sync.WaitGroup{},
 	}
 
 	result := Ranges{}
 
 	for it() {
-		err := hc.decomposeRegion(0, cell.Clone(), &dc, &result)
+		err := hc.decomposeRanges(0, cell.Clone(), &dc, &result)
 		if err != nil {
 			return Ranges{}, err
 		}
@@ -108,7 +104,7 @@ func (hc *Hilbert) DecomposeRegion(minTier, maxTier uint32,
 	return result, nil
 }
 
-func (hc *Hilbert) decomposeRegion(tier uint32, cell Point, dc *decomposeCall, result *Ranges) error {
+func (hc *Hilbert) decomposeRanges(tier uint32, cell Point, dc *decomposeCall, result *Ranges) error {
 
 	tierBit := Bitmask(1) << (Bitmask(hc.order) - Bitmask(tier) - 1)
 	upperBits := tierBit - 1
@@ -137,19 +133,7 @@ func (hc *Hilbert) decomposeRegion(tier uint32, cell Point, dc *decomposeCall, r
 
 			// if we've reached the max tier, or are fully contained
 			if tier == dc.maxTier || contains {
-				//tmp2 := upper.Clone()
 
-				// lower, err := BBoxLowerValue(Bitmask(hc.order), tmp1, tmp2)
-				// if err != nil {
-				// 	return err
-				// }
-
-				// copy(tmp1, cell)
-				// copy(tmp2, upper)
-				// upper, err := BBoxUpperValue(Bitmask(hc.order), tmp1, tmp2)
-				// if err != nil {
-				// 	return err
-				// }
 				value := Encode(Bitmask(hc.order), cell)
 				tierValueBits := Bitmask(1) << ((hc.order - tier - 1) * hc.dim)
 				tierValueBits--
@@ -159,9 +143,102 @@ func (hc *Hilbert) decomposeRegion(tier uint32, cell Point, dc *decomposeCall, r
 					MaxValue: value | tierValueBits,
 				}
 				*result = append(*result, r)
+			} else {
 				// if we only partially overlap and we aren't at the max
 				// tier
+
+				it := hc.cellIterator(tier+1, cell)
+				// go through all the child cells at this tier
+				for it() {
+					hc.decomposeRanges(tier+1, cell, dc, result)
+				}
+			}
+			// if we aren't in the reporting range, just recurse
+		} else {
+			it := hc.cellIterator(tier+1, cell)
+			// go through all the child cells at this tier
+			for it() {
+				hc.decomposeRanges(tier+1, cell, dc, result)
+			}
+		}
+	}
+
+	return nil
+}
+
+// DecomposeRegion breaks a region up into a series of hilbert value cells.
+//
+// minTier - The minimum tier in the hilbert curve to start the decomposition.
+// Setting this too high may result in a large number of ranges.
+//
+// maxTier - The maximum tier to recurse down to during the decomposition.
+// Setting maxTier to a high value may results in a very large number of
+// ranges.
+func (hc *Hilbert) DecomposeRegion(minTier, maxTier uint32,
+	region Intersecter) ([]Cell, error) {
+
+	cell := make(Point, hc.dim, hc.dim)
+	it := hc.cellIterator(0, cell)
+
+	dc := decomposeCall{
+		bounds:  Bounds{Min: cell.Clone(), Max: cell.Clone()},
+		minTier: minTier,
+		maxTier: maxTier,
+		region:  region,
+	}
+
+	result := []Cell{}
+
+	for it() {
+		err := hc.decomposeRegion(0, cell.Clone(), &dc, &result)
+		if err != nil {
+			return []Cell{}, err
+		}
+	}
+
+	return result, nil
+}
+
+func (hc *Hilbert) decomposeRegion(tier uint32, cell Point, dc *decomposeCall, result *[]Cell) error {
+
+	tierBit := Bitmask(1) << (Bitmask(hc.order) - Bitmask(tier) - 1)
+	upperBits := tierBit - 1
+
+	// calculate the upper bound
+	copy(dc.bounds.Min, cell)
+	copy(dc.bounds.Max, cell)
+	for d := uint32(0); d < hc.dim; d++ {
+		dc.bounds.Max[d] |= upperBits
+	}
+
+	intersects, err := dc.region.Intersects(&dc.bounds)
+	if err != nil {
+		return err
+	}
+	// if the region intersects the bounds of this tier/cell
+	if intersects {
+
+		// if we're in the reporting range
+		if tier >= dc.minTier {
+
+			contains, err := dc.region.Contains(&dc.bounds)
+			if err != nil {
+				return err
+			}
+
+			// if we've reached the max tier, or are fully contained
+			if tier == dc.maxTier || contains {
+				tmp := make([]Bitmask, hc.dim, hc.dim)
+				for i := range cell {
+					tmp[i] = cell[i] >> (hc.order - tier - 1)
+				}
+
+				value := Encode(Bitmask(tier+1), tmp)
+				*result = append(*result, Cell{Value: value, Tier: tier})
 			} else {
+				// if we only partially overlap and we aren't at the max
+				// tier
+
 				it := hc.cellIterator(tier+1, cell)
 				// go through all the child cells at this tier
 				for it() {
@@ -179,152 +256,4 @@ func (hc *Hilbert) decomposeRegion(tier uint32, cell Point, dc *decomposeCall, r
 	}
 
 	return nil
-}
-
-// DecomposeRegionThreads breaks a region up into a series of hilbert value
-// ranges.
-//
-// This is similar to DecomposeRegion, but multiple threads are used. In simple
-// cases this is a little slower and uses far more CPUs, but if contains and
-// intersects are expesive operations this may provide some performance
-// improvement.
-//
-// minTier - The minimum tier in the hilbert curve to start the decomposition.
-// Setting this too high may result in a large number of ranges.
-//
-// maxTier - The maximum tier to recurse down to during the decomposition.
-// Setting maxTier to a high value may results in a very large number of
-// ranges.
-func (hc *Hilbert) DecomposeRegionThreads(minTier, maxTier uint32,
-	region Intersecter) (Ranges, error) {
-
-	cell := make(Point, hc.dim, hc.dim)
-	it := hc.cellIterator(0, cell)
-
-	dc := decomposeCall{
-		err:     make(chan error),
-		minTier: minTier,
-		maxTier: maxTier,
-		ranges:  make(chan Range),
-		region:  region,
-		wg:      &sync.WaitGroup{},
-	}
-
-	go func() {
-		for it() {
-			dc.wg.Add(1)
-			go hc.decomposeRegionThreads(0, cell.Clone(), &dc)
-		}
-
-		dc.wg.Wait()
-
-		close(dc.ranges)
-		close(dc.err)
-	}()
-
-	result := Ranges{}
-
-	for {
-		select {
-		case r, ok := <-dc.ranges:
-			if !ok {
-				dc.ranges = nil
-			} else {
-				result = append(result, r)
-			}
-		case err, ok := <-dc.err:
-			if !ok {
-				dc.err = nil
-			} else {
-				return Ranges{}, err
-			}
-		}
-
-		if dc.ranges == nil && dc.err == nil {
-			break
-		}
-	}
-
-	result = joinRanges(result)
-
-	return result, nil
-}
-
-func (hc *Hilbert) decomposeRegionThreads(tier uint32, cell Point, dc *decomposeCall) {
-
-	defer dc.wg.Done()
-
-	tierBit := Bitmask(1) << (Bitmask(hc.order) - Bitmask(tier) - 1)
-	upperBits := tierBit - 1
-	upper := make(Point, hc.dim, hc.dim)
-
-	// calculate the upper bound
-	copy(upper, cell)
-	for d := uint32(0); d < hc.dim; d++ {
-		upper[d] |= upperBits
-	}
-
-	bounds := &Bounds{Min: cell, Max: upper}
-
-	intersects, err := dc.region.Intersects(bounds)
-	if err != nil {
-		dc.err <- err
-		return
-	}
-	// if the region intersects the bounds of this tier/cell
-	if intersects {
-
-		// if we're in the reporting range
-		if tier >= dc.minTier {
-
-			contains, err := dc.region.Contains(bounds)
-			if err != nil {
-				dc.err <- err
-				return
-			}
-
-			// if we've reached the max tier, or are fully contained
-			if tier == dc.maxTier || contains {
-
-				lower, err := BBoxLowerValue(Bitmask(hc.order), cell.Clone(), upper.Clone())
-				if err != nil {
-					dc.err <- err
-					return
-				}
-
-				upper, err := BBoxUpperValue(Bitmask(hc.order), cell.Clone(), upper.Clone())
-				if err != nil {
-					dc.err <- err
-					return
-				}
-
-				r := Range{
-					MinValue: lower,
-					MaxValue: upper,
-				}
-				dc.ranges <- r
-				// if we only partially overlap and we aren't at the max
-				// tier
-			} else {
-				c := cell.Clone()
-				it := hc.cellIterator(tier+1, c)
-				// go through all the child cells at this tier
-				for it() {
-					dc.wg.Add(1)
-					go hc.decomposeRegionThreads(tier+1, c.Clone(), dc)
-				}
-			}
-			// if we aren't in the reporting range, just recurse
-		} else {
-			c := cell.Clone()
-			it := hc.cellIterator(tier+1, c)
-			// go through all the child cells at this tier
-			for it() {
-				dc.wg.Add(1)
-				go hc.decomposeRegionThreads(tier+1, c.Clone(), dc)
-			}
-		}
-	}
-
-	return
 }
--- hilbertrange_test.go
+++ hilbertrange_test.go
@@ -24,7 +24,7 @@ func bruteAllValues(bounds sfc.Bounds, coord sfc.Point, hc *sfc.Hilbert,
 	}
 }
 
-func TestHilbertDecomposeRegion(t *testing.T) {
+func TestHilbertDecomposeRanges(t *testing.T) {
 
 	type tcase struct {
 		dim      uint32
@@ -41,7 +41,7 @@ func TestHilbertDecomposeRegion(t *testing.T) {
 			t.Fatalf("error creating hilbert curve, %v", err)
 		}
 
-		result, err := uut.DecomposeRegion(tc.minTier, tc.maxTier, tc.bounds)
+		result, err := uut.DecomposeRanges(tc.minTier, tc.maxTier, tc.bounds)
 		if err != nil {
 			t.Fatalf("error decomposing region, %v", err)
 		}
@@ -53,7 +53,6 @@ func TestHilbertDecomposeRegion(t *testing.T) {
 		}
 	}
 
-	// results were verified against the original C code.
 	tcases := map[string]tcase{
 		"test1": {
 			dim:     2,
@@ -97,7 +96,7 @@ func TestHilbertDecomposeRegion(t *testing.T) {
 	}
 }
 
-func BenchmarkHilbertDecomposeRegion(b *testing.B) {
+func BenchmarkHilbertDecomposeRanges(b *testing.B) {
 
 	uut, err := sfc.NewHilbert(2, 32)
 	if err != nil {
@@ -105,7 +104,7 @@ func BenchmarkHilbertDecomposeRegion(b *testing.B) {
 	}
 
 	for i := 0; i < b.N; i++ {
-		_, err := uut.DecomposeRegion(0, 32, &sfc.Bounds{
+		_, err := uut.DecomposeRanges(0, 32, &sfc.Bounds{
 			Min: sfc.Point{32000, 35000},
 			Max: sfc.Point{45000, 38000},
 		})
@@ -115,11 +114,11 @@ func BenchmarkHilbertDecomposeRegion(b *testing.B) {
 	}
 }
 
-// TestHilbertDecomposeRegion2 uses brute force to extract all values in a
+// TestHilbertDecomposeRanges2 uses brute force to extract all values in a
 // range and then compares them against the ranges returned. This will only
 // flag an error if a range doesn't contain a value, not if it contains too
 // many values.
-func TestHilbertDecomposeRegion2(t *testing.T) {
+func TestHilbertDecomposeRanges2(t *testing.T) {
 
 	type tcase struct {
 		dim     uint32
@@ -135,7 +134,7 @@ func TestHilbertDecomposeRegion2(t *testing.T) {
 			t.Fatalf("error creating hilbert curve, %v", err)
 		}
 
-		result, err := uut.DecomposeRegion(tc.minTier, tc.maxTier, tc.bounds)
+		result, err := uut.DecomposeRanges(tc.minTier, tc.maxTier, tc.bounds)
 		if err != nil {
 			t.Fatalf("error decomposing region, %v", err)
 		}
@@ -159,7 +158,6 @@ func TestHilbertDecomposeRegion2(t *testing.T) {
 		}
 	}
 
-	// results were verified against the original C code.
 	tcases := map[string]tcase{
 		"test1": {
 			dim:     2,
@@ -180,6 +178,77 @@ func TestHilbertDecomposeRegion2(t *testing.T) {
 				Min: []sfc.Bitmask{2, 1, 2},
 				Max: []sfc.Bitmask{4, 5, 7},
 			},
+		},
+	}
+
+	for k, v := range tcases {
+		tc := v
+		t.Run(k, func(t *testing.T) { fn(t, tc) })
+
+	}
+}
+
+func TestHilbertDecomposeRegion(t *testing.T) {
+
+	type tcase struct {
+		dim      uint32
+		order    uint32
+		minTier  uint32
+		maxTier  uint32
+		bounds   sfc.Intersecter
+		expected []sfc.Cell
+	}
+
+	fn := func(t *testing.T, tc tcase) {
+		uut, err := sfc.NewHilbert(tc.dim, tc.order)
+		if err != nil {
+			t.Fatalf("error creating hilbert curve, %v", err)
+		}
+
+		result, err := uut.DecomposeRegion(tc.minTier, tc.maxTier, tc.bounds)
+		if err != nil {
+			t.Fatalf("error decomposing region, %v", err)
+		}
+
+		// order doesn't matter so try sorting if this test ever fails
+		if reflect.DeepEqual(result, tc.expected) == false {
+			t.Errorf("invalid result, expected %v got %v", tc.expected, result)
+		}
+	}
+
+	tcases := map[string]tcase{
+		"test1": {
+			dim:     2,
+			order:   3,
+			minTier: 0,
+			maxTier: 0,
+			bounds: &sfc.Bounds{
+				Min: []sfc.Bitmask{2, 1},
+				Max: []sfc.Bitmask{4, 5},
+			},
+			expected: []sfc.Cell{{Value: 0, Tier: 0}, {Value: 3, Tier: 0}, {Value: 1, Tier: 0}, {Value: 2, Tier: 0}},
+		},
+		"test2": {
+			dim:     2,
+			order:   3,
+			minTier: 0,
+			maxTier: 1,
+			bounds: &sfc.Bounds{
+				Min: []sfc.Bitmask{3, 4},
+				Max: []sfc.Bitmask{7, 7},
+			},
+			expected: []sfc.Cell{{Value: 7, Tier: 1}, {Value: 6, Tier: 1}, {Value: 2, Tier: 0}},
+		},
+		"test3": {
+			dim:     3,
+			order:   3,
+			minTier: 0,
+			maxTier: 2,
+			bounds: &sfc.Bounds{
+				Min: []sfc.Bitmask{1, 2, 3},
+				Max: []sfc.Bitmask{1, 2, 4},
+			},
+			expected: []sfc.Cell{{Value: 48, Tier: 2}, {Value: 123, Tier: 2}},
 		},
 	}
 
